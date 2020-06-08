e3dd4227e46a51a6f58a9ce4871d46daabe9dd3a
Hussein Talal <hossientalal2@gmail.com>
2020-06-08 18:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function wrapAdjective(flair = "*"){
     return `You are ${flair}${para}${flair}!`
   }
 }
+let Calculator = {
+  add : function (x,y){
+    return x + y;
+  },
+  subtract : function (x,y){
+    return x - y;
+  },
+  multiply : function (x,y){
+    return x * y;
+  },
+  divide : function (x,y){
+    return x / y;
+  }
+};
