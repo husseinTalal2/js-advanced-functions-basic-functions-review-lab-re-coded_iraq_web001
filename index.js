35dd9770fead176df639c18ce95bbe3525478541
Hussein Talal <hossientalal2@gmail.com>
2020-06-08 18:51:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(action = "roller-skate"){
+  return `This Saturday, I want to ${action}!`
+}
+function mondayWork(action = "go to the office"){
+  return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(flair = "*"){
+  return function(para = "special"){
+    return `You are ${flair}${para}${flair}!`
+  }
+}
